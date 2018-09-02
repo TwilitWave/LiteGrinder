f575823f5b2bfd60c2ecee11a581dee4faec1f13
gtf5022 <gtf5022@gmail.com>
2018-09-02 15:34:33 -0600

    Demo Level 4 WIP
    Added Demo Level 4, Still Creating

--- LiteGrinder/PhysicsTesting/Object/demoLevelOne.cs
+++ LiteGrinder/PhysicsTesting/Object/demoLevelOne.cs
@@ -83,5 +83,27 @@ namespace LiteGrinder
             Line.ClearGhostLine();
         }
 
+        public void DemoStage4(World world)
+        {
+            player.SetStartPosition(ConvertUnits.ToSimUnits(new Vector2(150, 50)));
+            player.SetGoalPosition(ConvertUnits.ToSimUnits(new Vector2(1870, 520)));
+
+            wallTile = Content.Load<Texture2D>("wallTile");
+            new Block(world, new Vector2(0f, 6f), ConvertUnits.ToSimUnits(150), ConvertUnits.ToSimUnits(50), 1f, 0, BodyType.Static, wallTile);
+            new Block(world, new Vector2(4f, 6f), ConvertUnits.ToSimUnits(350), ConvertUnits.ToSimUnits(50), 1f, 0, BodyType.Static, wallTile);
+            new Block(world, new Vector2(6f, 9f), ConvertUnits.ToSimUnits(50), ConvertUnits.ToSimUnits(550), 1f, 0, BodyType.Static, wallTile);
+            new Block(world, new Vector2(9f, 7f), ConvertUnits.ToSimUnits(50), ConvertUnits.ToSimUnits(850), 1f, 0, BodyType.Static, wallTile);
+
+            strawberry = Content.Load<Texture2D>("strawberry");
+            new CollectableItem(world, 30, 2f, new Vector2(4f, 8f), BodyType.Static, strawberry);
+            new CollectableItem(world, 30, 2f, new Vector2(10f, 3f), BodyType.Static, strawberry);
+            new CollectableItem(world, 30, 2f, new Vector2(15f, 7f), BodyType.Static, strawberry);
+
+            noDrawSprite = Content.Load<Texture2D>("redTransparent");
+            new NoDrawArea(world, new Vector2(10f, 3f), ConvertUnits.ToSimUnits(75), ConvertUnits.ToSimUnits(800), 0, noDrawSprite);
+
+            Line.ClearGhostLine();
+        }
+
     }
 }
